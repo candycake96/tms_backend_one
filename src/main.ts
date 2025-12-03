15cf05ac8ca02e854dd21e2e6b7899a9e7918277
candycake <candycake96@gmail.com>
2025-12-03 17:08:38 +0700

    add db

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // เปิด CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // อนุญาตเฉพาะ frontend ของคุณ
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // ถ้าต้องใช้ cookie หรือ Authorization header
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
